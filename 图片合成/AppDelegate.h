5a47532700305078bd05b64799f7858cf1da3d61
Moker_C <15216656514@163.com>
2018-06-20 15:12:54 +0800

    Initial Commit

---
+++ 图片合成/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图片合成
+//
+//  Created by Mac1 on 2018/6/20.
+//  Copyright © 2018年 Mac1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 图片合成/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  图片合成
+//
+//  Created by Mac1 on 2018/6/20.
+//  Copyright © 2018年 Mac1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
